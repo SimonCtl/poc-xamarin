15f83378728a482e8789331422719b3deb63528b
Simon COTELLE <Simon.COTELLE@diiage.org>
2020-12-10 14:14:38 +0100

    POC v1

---
+++ PocDiiageTemplate/PocDiiageTemplate/ViewModels/UserPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocDiiageTemplate.ViewModels
+{
+    public class UserPageViewModel : ViewModelBase
+    {
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { SetProperty(ref _username, value); }
+        }
+
+        public UserPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "User Page";
+            Username = "Simon Cotelle";
+        }
+    }
+}
